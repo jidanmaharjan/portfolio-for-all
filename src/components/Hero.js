d371bf11b8eba6e10a8a6ba3535b369bac93668b
Jidan Maharjan <jidanmaharjan1@gmail.com>
2023-02-02 23:42:36 +0545

    dipak portfolio

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,52 +1,99 @@
 import React from "react";
+import Lottie from "react-lottie-player";
+import ReactTyped from "react-typed";
 
 //icons
 import { AiFillGithub, AiFillLinkedin, AiOutlineTwitter } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { FaKaggle } from "react-icons/fa";
 
 //image
-import hero from "../assets/hero.png";
+import hero from "../assets/hero.jpg";
+import hand from "../assets/hand.json";
 
 const Hero = () => {
   return (
-    <section className="text-gray-600 body-font pt-8">
-      <div className="container md:mx-auto flex md:px-12 p-4 py-20 md:pb-8 md:flex-row flex-col-reverse items-center">
-        <div className="lg:flex-grow self-start md:w-1/2 lg:pr-24 md:pr-16 flex flex-col items-start md:text-left mb-16 md:mb-0  text-center">
-          <p className="mb-4 leading-relaxed text-2xl 2xl:text-4xl text-gray-400 md:mt-12">
-            HEY THERE !
+    <section
+      id="particles-js"
+      className="text-gray-600 body-font dark:bg-slate-800 bg-gray-100"
+    >
+      <div className="container md:mx-auto  flex md:px-12 p-4 py-20 md:pb-8 min-h-screen md:flex-row flex-col-reverse justify-center items-center">
+        <div className="lg:flex-grow w-full md:w-1/2 lg:pr-24 md:pr-16 flex flex-col items-start md:text-left mb-16 md:mb-0  text-center">
+          <p className="mb-4 leading-relaxed text-2xl 2xl:text-4xl text-gray-400 md:mt-12 flex items-center">
+            HEY THERE{" "}
+            <Lottie
+              loop
+              animationData={hand}
+              play
+              style={{ width: 60, height: 60 }}
+            />{" "}
+            !
           </p>
-          <h1 className="title-font sm:text-7xl text-3xl 2xl:text-8xl md:mb-4 font-semibold text-red-500">
-            I AM SUMITRA
+          <h1 className="title-font sm:text-7xl text-3xl 2xl:text-8xl md:mb-4 font-semibold text-blue-500">
+            <ReactTyped
+              className=""
+              strings={["WHO AM I ?", "I AM DIPAK CRESTHA"]}
+              typeSpeed={120}
+              backSpeed={80}
+              backDelay={2000}
+              loop
+            />
           </h1>
           <p className="mb-8 leading-relaxed text-2xl 2xl:text-5xl sm:text-3xl font-semibold">
-            QA ENGINEER
+            ML ENGINEER
           </p>
           <div className="flex items-center text-3xl 2xl:text-4xl mt-4 2xl:mt-8 text-gray-400">
-            <a className="mr-4 hover:text-red-400" href="#">
+            <a
+              className="mr-4 hover:text-blue-500"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/shresthadipak"
+            >
               <AiFillGithub />
             </a>
-            <a className="mr-4 hover:text-red-400" href="#">
+            <a
+              className="mr-4 hover:text-blue-500"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/dipak-shrestha-99897311a/"
+            >
               <AiFillLinkedin />
             </a>
-            <a className="mr-4 hover:text-red-400" href="#">
+            <a
+              className="mr-4 hover:text-blue-500"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://twitter.com/shresthadipak4"
+            >
               <AiOutlineTwitter />
+            </a>
+            <a
+              className="mr-4 hover:text-blue-500"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.kaggle.com/shresthadipak"
+            >
+              <FaKaggle />
             </a>
           </div>
           <div className="flex justify-center mt-12 2xl:mt-16">
-            <Link
-              to={"/services"}
-              className="inline-flex 2xl:text-2xl text-white bg-red-400 border-0 py-2 px-6 focus:outline-none hover:bg-red-500 rounded text-lg"
+            <a
+              href="https://www.blog.shresthadipak.com.np/"
+              target="_blank"
+              norefferer
+              className="inline-flex 2xl:text-2xl text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-400 rounded text-lg"
             >
-              CHECK MY SERVICES
-            </Link>
+              CHECK MY BLOG
+            </a>
           </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2  blob--object bg-red-400 overflow-hidden pointer-events-none mb-8 md:mb-0">
-          <img
-            className="object-cover object-center rounded"
-            alt="hero"
-            src={hero}
-          />
+        <div className="lg:max-w-lg lg:w-full md:w-1/2  blob--object bg-gradient-to-r from-violet-400 to-blue-400  pointer-events-none mb-8 md:mb-0">
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 blob--object--small  overflow-hidden pointer-events-none mb-8 md:mb-0">
+            <img
+              className="object-cover object-center rounded aspect-square"
+              alt="hero"
+              src={hero}
+            />
+          </div>
         </div>
       </div>
     </section>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,65 +1,31 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
 
-import s from "../assets/s.png";
+import s from "../assets/logod.png";
 
 //icons
 import { FiSend } from "react-icons/fi";
 
-const Navbar = ({ navToggle, setNavToggle }) => {
-  const activeLink = "mr-5 mb-4 md:mb-0 text-red-200 md:text-red-400";
-  const inActiveLink = "mr-5 mb-4 md:mb-0 hover:text-red-500";
+const Navbar = () => {
+  const activeLink = "mr-5 mb-4 md:mb-0 text-red-200 md:text-blue-400";
+  const inActiveLink = "mr-5 mb-4 md:mb-0 hover:text-blue-500";
   return (
     <header
-      className={`text-white md:text-gray-600 body-font fixed right-0 md:top-0 bg-red-500 md:bg-white w-60 md:w-full transition-all duration-300 z-40 ${
-        !navToggle && "translate-x-[100%] md:translate-x-0"
-      }`}
+      className={`text-white md:text-gray-600 body-font fixed top-0  bg-white w-full z-40 `}
     >
-      <div className="container mx-auto flex flex-wrap p-4 md:p-5  flex-col md:flex-row md:items-center h-screen md:h-fit w-60 md:w-full">
-        <Link
-          to={"/"}
-          onClick={() => setNavToggle(false)}
-          className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+      <div className="container mx-auto flex flex-wrap p-4 md:p-5 flex-row items-center h-fit w-full">
+        <a
+          href=""
+          className="flex title-font font-medium items-center text-gray-900  w-fit"
         >
-          <img className="w-10 h-10 rounded-full" src={s} alt="" />
-          <span className="ml-3 text-xl font-semibold text-red-400 hover:text-red-300">
+          <img className="w-10 h-10 " src={s} alt="" />
+          <span className="ml-3 text-xl font-semibold text-blue-500 hover:text-blue-400">
             Portfolio.
           </span>
-        </Link>
-        <nav className="md:ml-auto md:mr-auto flex flex-col md:flex-row flex-wrap items-start md:items-center text-base justify-center">
-          <NavLink
-            onClick={() => setNavToggle(false)}
-            to={"/"}
-            className={({ isActive }) => (isActive ? activeLink : inActiveLink)}
-          >
-            Home
-          </NavLink>
-          <NavLink
-            onClick={() => setNavToggle(false)}
-            to={"/about"}
-            className={({ isActive }) => (isActive ? activeLink : inActiveLink)}
-          >
-            About
-          </NavLink>
-          <NavLink
-            onClick={() => setNavToggle(false)}
-            to={"/services"}
-            className={({ isActive }) => (isActive ? activeLink : inActiveLink)}
-          >
-            Services
-          </NavLink>
-          <NavLink
-            onClick={() => setNavToggle(false)}
-            to={"/contact"}
-            className={({ isActive }) => (isActive ? activeLink : inActiveLink)}
-          >
-            Contact
-          </NavLink>
-        </nav>
+        </a>
+
         <a
-          onClick={() => setNavToggle(false)}
-          href="mailto:sumitramhrjn@gmail.com"
-          className="inline-flex items-center border md:border-red-400 md:hover:border-red-500 bg-transparent py-2 px-3 focus:outline-none md:text-red-400 md:hover:text-red-500 rounded text-base mt-4 md:mt-0"
+          href="mailto:shresthadipak2049@gmail.com"
+          className="inline-flex items-center ml-auto border border-blue-500 hover:border-blue-400 bg-transparent py-2 px-3 focus:outline-none text-blue-500 hover:text-blue-400 rounded text-base "
         >
           Lets Talk
           <FiSend className="ml-2" />
